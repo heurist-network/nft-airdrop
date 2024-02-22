46d6db14e0da84e36d929365284205e6cf144a06
JW <artoriamaster@gmail.com>
2024-02-22 14:08:30 -0800

    feat: signing script

---
+++ airdrop-signer/index.js
@@ -0,0 +1,33 @@
+const { ethers } = require("ethers");
+const claimantAddresses = require('./addresses.json');
+const fs = require('fs');
+
+// Usage: bun run index.js
+
+// Admin wallet private key
+const adminPrivateKey = process.env.ADMIN_PRIVATE_KEY;
+const adminWallet = new ethers.Wallet(adminPrivateKey)
+
+async function signClaimantAddresses() {
+    const results = []
+    for (let address of claimantAddresses) {
+        const hash = ethers.keccak256(ethers.AbiCoder.defaultAbiCoder().encode(["address"], [address]));
+        const signature = await adminWallet.signMessage(ethers.getBytes(hash));
+
+        console.log(`Address: ${address}`);
+        console.log(`Hash: ${hash}`);
+        console.log(`Signature: ${signature}`);
+        console.log('-----------------------');
+
+        results.push({
+            address:address.toLowerCase(),
+            hash,
+            signature
+        });
+    }
+
+    // Save the results to a file
+    fs.writeFileSync('signatures.json', JSON.stringify(results, null, 2));
+}
+
+signClaimantAddresses().catch(console.error);
